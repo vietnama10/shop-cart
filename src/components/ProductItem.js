e725c2546672053ddace815fad2a2ec8f1e08a92
unknown <dunglq@DUNGLQ-PC.unitech.net>
2019-01-15 14:00:46 +0700

    animation addtocart

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 
 const ProductItem = ({ product, onAddToCart }) => {
+  const addToCart_Combine_Animation = () => {
+    onAddToCart();
+    let imgAnimation = document.getElementById("addtocart-" + product.id);
+    imgAnimation.classList.add("adding");
+    let productItem = document.getElementById("addtocart-" + product.id).parentNode;
+    let imgAnimationClone = document.getElementById("addtocart-" + product.id).cloneNode(true);
+    imgAnimation.parentNode.removeChild(imgAnimation);
+    productItem.appendChild(imgAnimationClone);
+  }
   return (
     <div className="card-container col-sm-3">
       <div className="card product-item">
@@ -20,7 +29,7 @@ const ProductItem = ({ product, onAddToCart }) => {
         <div className="card-body">
           <h6 className="card-title">{product.name}</h6>
           <p className="card-text">${product.price}</p>
-          <button onClick={onAddToCart} className="btn btn-primary card-action">Add</button>
+          <button onClick={addToCart_Combine_Animation} className="btn btn-primary card-action">Add</button>
         </div>
       </div>
     </div>
